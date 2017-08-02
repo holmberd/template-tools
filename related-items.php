c936be544e7de3d98245d82a067f83efcc335299
Dag Holmberg <holmberd@users.noreply.github.com>
2017-08-02 15:44:53 -0700

    Update related-items.php

--- related-items.php
+++ related-items.php
@@ -17,17 +17,17 @@ function getAllPosts() {
 	$posts = [];
 	$post = [];
 	$icon_array = null;
-	foreach ($ids as $id):
+	foreach ($ids as $id) {
 		$icon_array = get_field('icon', $id);
-		if (array_key_exists('url', $icon_array)):
+		if (array_key_exists('url', $icon_array)) {
 			$post['icon'] = $icon_array['url'];
-		endif;
+		}
 		$post['title'] = get_field('page_title', $id);
 		$post['category'] = get_field('category', $id);
 		$post['sub_title'] = get_field('page_sub-title', $id);
 		$post['rating'] = get_field('rating', $id);
 		$posts[] = $post;
-	endforeach;
+	}
 	return $posts;
 }
 
